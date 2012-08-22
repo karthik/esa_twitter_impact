c1e715a0acd34dd736087cba8a41bb838c096ed0
Karthik Ram <karthik.ram@gmail.com>
2012-08-22 14:54:29 -0700

    First pass at ESA twitter impact:q

---
+++ tweet_analysis.md
@@ -0,0 +1,31 @@
+# A few summary stats from ESA 2012
+
+<!-- read external r code -->
+
+
+
+# Cumulative Exposure of ESA 2012 tweets
+
+![plot of chunk plot-fig0](figure/plot-fig0.png) 
+
+
+# Total ESA2012 tweets
+![plot of chunk plot-fig0a](figure/plot-fig0a.png) 
+
+
+# Tweets by sentiment
+![plot of chunk plot-fig3](figure/plot-fig3.png) 
+
+
+# Mentions of ESA2012 over time scaled by impact
+![plot of chunk plot-fig1](figure/plot-fig1.png) 
+
+# Tweets by country
+![plot of chunk plot-fig2](figure/plot-fig2.png) 
+
+# Popular tags associated with ESA 2012
+
+![plot of chunk plot-fig4](figure/plot-fig4.png) 
+
+ 
+
---
+++ tweet_stats.R
@@ -0,0 +1,89 @@
+library(ggplot2)
+library(plyr)
+library(stringr)
+library(maps)
+library(lubridate)
+setwd('~/Desktop/Tweet_analysis')
+
+# A time function to convert unix timestamp to something more useful.
+convert_time <- function(time) {
+as.POSIXlt(time, origin="1970-01-01", tz="UTC")
+}
+
+
+## @knitr  cum_exposure
+# Cumulative exposure of ESA tweets
+cum_exposure <- read.csv('data/cumulativeexposure.csv', header = T)
+cum_exposure$time <- convert_time(cum_exposure$timestamp)
+cum_exposure <- cum_exposure[, -c(1:2)]
+
+
+## @knitr cumplot
+cumulative_plot <- ggplot(cum_exposure, aes(time, cumulative_exposure)) + geom_line(size=1, colour="#18699c") + geom_point(colour="gray") + ylab("Cumulative exposure") + xlab("Date") + opts(title="Cumulative exposure of ESA tweets")
+print(cumulative_plot)
+
+
+## @knitr  activity_history
+# Activity history
+ahistory <- read.csv('data/activityhistory.csv', header = T)
+ahistory <- ahistory[, -c(1:2)]
+ahistory$time <- convert_time(ahistory$timestamp)
+
+## @knitr aplot
+hist_plot <- ggplot(ahistory, aes(time, mentions)) + geom_line() + geom_point(aes(size=1.1+influential)) + opts(title="Mentions of ESA 2012 over time")
+print(hist_plot)
+
+## @knitr  discovery
+# discovery
+discovery <- read.csv('data/discovery.csv', header = T)
+discovery_data <- discovery[,1:33]
+discovery_table <- discovery[,c(1,2,34:51)]
+d_data <- melt(discovery_data, id.vars=1:2)
+d_data$variable <- str_sub(d_data$variable, 2)
+d_data$variable <- mdy(d_data$variable)
+d_data$value <- as.numeric(d_data$value)
+
+## @knitr dplot
+discovery_plot <- ggplot(subset(d_data, related.term!=""), aes(variable, value)) + geom_line() + facet_wrap(~ related.term, scales="free_y") + opts(axis.text.x = theme_text(angle =-90))
+print(discovery_plot)
+
+## @knitr  geoanalysis
+# geoanalysis
+geodata <- read.csv('data/geoanalysis.csv', header = T)
+geodata <- subset(geodata, Mentions > 0)
+geodata <- subset(geodata, Region !="World")
+geodata <- subset(geodata, Region !="Virgin Islands")
+
+## @knitr geoplot
+geoplot <- ggplot(subset(geodata, Query=="#esa2012"), aes(Region, Mentions)) + geom_point() + opts(axis.text.x = theme_text(angle =-90))
+print(geoplot)
+
+## @knitr  sentiment
+# Sentiment of ESA tweeters
+sentiment <- read.csv('data/sentiment.csv', header = T)
+sentiment <- sentiment[, -c(1:2)]
+sentiment$date <- convert_time(sentiment$timestamp)
+
+## @knitr splot
+sentiment_plot <- ggplot(sentiment, aes(date)) + opts(title="Sentiment of tweets under #ESA2012 hashtag")
+sentiment_plot +  geom_line(aes(y=positive_sentiment), colour="#18699c") +  geom_line(aes(y=negative_sentiment), colour="#a32e3a") + ylab("Count") + xlab("Time")
+
+## @knitr totalplot
+ggplot(sentiment, aes(date)) + opts(title="Number of tweets under #ESA2012 hashtag") +   geom_line(aes(y=mentions), colour="#474747") + ylab("Count") + xlab("Time")
+
+
+## @knitr  trending_links
+# trending links
+trending_links <- read.csv('data/trendinglinks.csv', header = T)
+
+## @knitr  trending_photos
+# trending photos
+trending_photos <- read.csv('data/trendingphotos.csv', header = T)
+
+## @knitr  trending_posts
+# trending posts 
+trending_posts <- read.csv('data/trendingposts.csv', header = T)
+
+# trending videos
+trending_videos <- read.csv('data/trendingvideos.csv', header = T)
+
