7365a4eb743a772d3ade0ea26bbb4e3512907cdf
Karthik Ram <karthik.ram@gmail.com>
2012-08-28 14:13:54 -0700

    removed old script.

--- all_tweets.R
+++
@@ -1,8 +0,0 @@
-# All tweets
-setwd('~/Dropbox/esa_twitter_impact/')
-all_tweets <- read.csv('data/all_tweets.csv', header=T)
-names(all_tweets)
-handles <- unique(all_tweets$user.screen_name)
-identities <- data.frame(twitter_handle=handles, gender=NA, position = NA, highest_degreee = NA)
-write.csv(identities, file="esa_tweets.csv")
-# This is the file with all unique twitter handles
