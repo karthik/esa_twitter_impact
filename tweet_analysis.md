fccf135cef18abbee480173e7ec80535052a9dd3
Karthik Ram <karthik.ram@gmail.com>
2012-08-28 11:18:27 -0700

    Updated figures and markdown

--- tweet_analysis.md
+++ tweet_analysis.md
@@ -1,110 +1,57 @@
-<style type="text/css">
-body {
-font-family: "Verdana";
-}
-</style>
+<!-- read external r code -->
+
+
 
 # Summary stats from the esa2012 tweets
 These are some summaries on tweets tagged `#esa2012` between mid-July and mid-August. There were approximately 4905 tweets about the conference (still waiting on the raw data) but I was able to scrape some analytics data from the [Topsy API](http://code.google.com/p/otterapi/). I have additional information on the most popular links, photos and videos from the conference but have not summarized those. 
 
 Please note that these results aren't ready to be shared yet (barely scratched the surface) but do watch the repository for updates.
 
-<!-- read external r code -->
-
-
 
 ## Cumulative Exposure of ESA 2012 tweets
 The cumulative number of impressions that tweets tagged `#esa2012` received between mid-July and now.
 
-
-```
-## Error: object 'cum_exposure' not found
-```
-
-```
-## Error: object 'cumulative_plot' not found
-```
+![plot of chunk plot-fig0](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig0.png) 
 
 
 ## Total ESA2012 tweets
 Ecologists tweeted ~4905 times from the last week of July to one week after the conference. Most number of tweets in a single day was Wednesday the 7th quite likely coinciding with Carly Strasser's very popular session.
 
-
-```
-## Error: object 'sentiment' not found
-```
+![plot of chunk plot-fig0a](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig0a.png) 
 
 
 ## Tweets by sentiment (blue = positive, red = negative)
 Tweets were categorized as positive or negative based on a standard set of keywords. 
 
-
-```
-## Error: object 'sentiment' not found
-```
+![plot of chunk plot-fig3](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig3.png) 
 
 
 ## Mentions of ESA2012 over time scaled by impact
 Tweets on a hourly timescale. High impact tweets are ones that were retweeted and favorited more than others. The lulls during the week correspond to night time hours.
 
-
-```
-## Error: object 'ahistory' not found
-```
-
-```
-## Error: object 'hist_plot' not found
-```
+![plot of chunk plot-fig1](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig1.png) 
 
 ## Tweets by country
 These are tweets and retweets by country. Not surprisingly most tweets originated from Portland but American ecologists also retweeted the most followed by the UK and Canada.
 
-
-```
-## Error: object 'geodata' not found
-```
-
-```
-## Error: object 'geoplot' not found
-```
+![plot of chunk plot-fig2](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig2.png) 
 
 ## Popular tags associated with ESA 2012
 These are tags that popped up as being popular alongside the esa2012 tag. Not sure why middleagedwhitedude and hawaiianshirt trended so much.
 
 
-
-```
-## Error: object 'd_data' not found
-```
-
-```
-## Error: object 'discovery_plot' not found
-```
+![plot of chunk plot-fig4](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig4.png) 
 
 ## R 
 R got mentioned consistently throughout the week and most on Thursday (when [rOpenSci](http://ropensci.org) held its workshop.) 
 
-
-```
-## Error: object 'd_data' not found
-```
-
-```
-## Error: object 'rstats' not found
-```
+![plot of chunk plot-fig5](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig5.png) 
 
  
 ## ESA Social 
 ESA Social also got most mentions on the day of the social media workshop (Tuesday)
 
-
-```
-## Error: object 'd_data' not found
-```
-
-```
-## Error: object 'esasocial' not found
-```
+![plot of chunk plot-fig6](https://raw.github.com/karthikram/esa_twitter_impact/master/figure/plot-fig6.png) 
 
  
 
--- tweet_stats.R
+++ tweet_stats.R
@@ -4,13 +4,13 @@ library(stringr)
 library(maps)
 library(lubridate)
 
-# A time function to convert unix timestamp to something more useful.
+## @knitr function
 convert_time <- function(time) {
 as.POSIXlt(time, origin="1970-01-01", tz="UTC")
 }
 
 
-## @knitr  cum_exposure
+## @knitr cum_exposure
 cum_exposure <- read.csv('data/cumulativeexposure.csv', header = T)
 cum_exposure$time <- convert_time(cum_exposure$timestamp)
 cum_exposure <- cum_exposure[, -c(1:2)]
@@ -21,7 +21,7 @@ cumulative_plot <- ggplot(cum_exposure, aes(time, cumulative_exposure)) + geom_l
 print(cumulative_plot)
 
 
-## @knitr  activity_history
+## @knitr activity_history
 ahistory <- read.csv('data/activityhistory.csv', header = T)
 ahistory <- ahistory[, -c(1:2)]
 ahistory$time <- convert_time(ahistory$timestamp)
@@ -30,7 +30,7 @@ ahistory$time <- convert_time(ahistory$timestamp)
 hist_plot <- ggplot(ahistory, aes(time, mentions)) + geom_line() + geom_point(aes(size=1.1+influential)) + opts(title="Mentions of ESA 2012 over time")
 print(hist_plot)
 
-## @knitr  discovery
+## @knitr discovery
 discovery <- read.csv('data/discovery.csv', header = T)
 discovery_data <- discovery[,1:33]
 discovery_table <- discovery[,c(1,2,34:51)]
@@ -43,7 +43,7 @@ d_data$value <- as.numeric(d_data$value)
 discovery_plot <- ggplot(subset(d_data, related.term!=""), aes(variable, value)) + geom_line() + facet_wrap(~ related.term, scales="free_y") + opts(axis.text.x = theme_text(angle =-90))
 print(discovery_plot)
 
-## @knitr  geoanalysis
+## @knitr geoanalysis
 geodata <- read.csv('data/geoanalysis.csv', header = T)
 geodata <- subset(geodata, Mentions > 0)
 geodata <- subset(geodata, Region !="World")
@@ -53,35 +53,25 @@ geodata <- subset(geodata, Region !="Virgin Islands")
 geoplot <- ggplot(subset(geodata, Query=="#esa2012"), aes(Region, Mentions)) + geom_point() + opts(axis.text.x = theme_text(angle =-90))
 print(geoplot)
 
-## @knitr  sentiment
+## @knitr sentiment
 sentiment <- read.csv('data/sentiment.csv', header = T)
 sentiment <- sentiment[, -c(1:2)]
 sentiment$date <- convert_time(sentiment$timestamp)
 
 ## @knitr splot
-ggplot(sentiment, aes(date)) + geom_line(aes(y=positive_sentiment), colour="#18699c") +  geom_line(aes(y=negative_sentiment), colour="#a32e3a") + ylab("Count") + xlab("Time")  + opts(title="Sentiment of tweets under #ESA2012 hashtag (blue = positive, red = negative)")
+ggplot(sentiment, aes(date)) + geom_line(aes(y=positive_sentiment), colour="#18699c",size=1.5) +  geom_line(aes(y=negative_sentiment), colour="#a32e3a",size=1.5) + ylab("Count") + xlab("Time")  + opts(title="Sentiment of tweets under #ESA2012 hashtag (blue = positive, red = negative)")
 
 ## @knitr totalplot
-ggplot(sentiment, aes(date)) + opts(title="Number of tweets under #ESA2012 hashtag") +   geom_line(aes(y=mentions), colour="#474747") + ylab("Count") + xlab("Time")
-
-
-## @knitr  trending_links
-trending_links <- read.csv('data/trendinglinks.csv', header = T)
-
-## @knitr  trending_photos
-trending_photos <- read.csv('data/trendingphotos.csv', header = T)
-
-## @knitr  trending_posts
-trending_posts <- read.csv('data/trendingposts.csv', header = T)
-
+ggplot(sentiment, aes(date)) + opts(title="Number of tweets under #ESA2012 hashtag") +   geom_line(aes(y=mentions), colour="#474747",size=1.5) + ylab("Count") + xlab("Time")
 
 ## @knitr rstats
 rstats <- subset(d_data, related.term=="#rstats")
-ggplot(rstats[15:20,], aes(variable, value)) + geom_line()  + opts(axis.text.x = theme_text(angle =-90))
-
+rstats_plot <- ggplot(rstats[15:20,], aes(variable, value)) + geom_line(size=1.5)  + opts(axis.text.x = theme_text(angle =-90))
+print(rstats_plot)
 
 
 ## @knitr esasocial
 esasocial <- subset(d_data, related.term=="#esasocial")
-ggplot(esasocial[15:20,], aes(variable, value)) + geom_line()  + opts(axis.text.x = theme_text(angle =-90))
+esasocial_plot <- ggplot(esasocial[15:20,], aes(variable, value)) + geom_line(size=1.5)  + opts(axis.text.x = theme_text(angle =-90))
+print(esasocial_plot)
 
